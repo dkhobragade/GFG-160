52700e49e1b66eb72af787ff5ad18d48ec6bdaa6
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-09 11:11:57 +0530

    Sort 0s 1s and 2s

---
+++ Sorting/Sort_0s_1s_and_2s.py
@@ -0,0 +1,19 @@
+arr = [0, 1, 2, 0, 1, 2]
+
+n = len(arr)
+low = 0
+mid = 0
+high = n - 1
+
+
+while mid <= high:
+    if arr[mid] == 0:
+        arr[mid], arr[low] = arr[low], arr[mid]
+        mid += 1
+        low += 1
+    elif arr[mid] == 1:
+        mid += 1
+    else:
+        arr[mid], arr[high] = arr[high], arr[mid]
+        high -= 1
+print(arr)
