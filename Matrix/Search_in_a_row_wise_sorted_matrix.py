90ca149509e60bdce3710b04e2c2b3e63b9e7524
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-27 10:43:51 +0530

    Refactor search algorithm for row-wise sorted matrix to use two-pointer technique

--- Matrix/Search_in_a_row_wise_sorted_matrix.py
+++ Matrix/Search_in_a_row_wise_sorted_matrix.py
@@ -2,28 +2,45 @@ mat = [[3, 4, 9], [2, 5, 6], [9, 25, 27]]
 x = 9
 
 
-def search(arr, x):
-    low = 0
-    high = len(arr) - 1
+# def search(arr, x):
+#     low = 0
+#     high = len(arr) - 1
 
-    while low <= high:
-        mid = low + (high - low) // 2
+#     while low <= high:
+#         mid = low + (high - low) // 2
 
-        if x == arr[mid]:
-            return True
+#         if x == arr[mid]:
+#             return True
 
-        elif x < arr[mid]:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return False
+#         elif x < arr[mid]:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return False
+
+
+# n = len(mat)
+# m = len(mat[0])
+
+# for i in range(n):
+#     if search(mat[i], x):
+#         print(True)
+#         break
+# print(False)
 
 
 n = len(mat)
 m = len(mat[0])
 
-for i in range(n):
-    if search(mat[i], x):
+i = 0
+j = m - 1
+
+
+while i < n and j >= 0:
+    if mat[i][j] == x:
         print(True)
         break
-print(False)
+    elif mat[i][j] > x:
+        j -= 1
+    else:
+        i += 1
