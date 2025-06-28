87fd12f992432a6c320dc61657194691cca3148a
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-28 10:21:23 +0530

    Refactor search implementation in a sorted matrix to use binary search

--- Matrix/Search_In_A_Sorted_Matrix.py
+++ Matrix/Search_In_A_Sorted_Matrix.py
@@ -1,19 +1,36 @@
 mat = [[87, 96, 99], [101, 103, 111]]
-x = 105
+x = 101
 
 
 n = len(mat)
 m = len(mat[0])
 
-i = 0
-j = m - 1
+# i = 0
+# j = m - 1
 
 
-while i < n and j >= 0:
-    if mat[i][j] == x:
+# while i < n and j >= 0:
+#     if mat[i][j] == x:
+#         print(True)
+#         break
+#     elif mat[i][j] < x:
+#         i += 1
+#     else:
+#         j -= 1
+
+low = 0
+high = n * m - 1
+
+while low <= high:
+    mid = low + (high - low) // 2
+
+    row = mid // m
+    col = mid % m
+
+    if mat[row][col] == x:
         print(True)
         break
-    elif mat[i][j] < x:
-        i += 1
+    elif mat[row][col] > x:
+        high = mid - 1
     else:
-        j -= 1
+        low = mid + 1
