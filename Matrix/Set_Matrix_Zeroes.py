8d9f253c1b3041bf0b0ec0ae5843756bc22023ed
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-29 12:06:02 +0530

    Refactor Set Matrix Zeroes implementation to streamline logic and remove unnecessary variables

--- Matrix/Set_Matrix_Zeroes.py
+++ Matrix/Set_Matrix_Zeroes.py
@@ -84,23 +84,53 @@ mat = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
 # print(mat)
 
 
+# n = len(mat)
+# m = len(mat[0])
+
+# row = [False] * n
+# col = [False] * m
+
+# for i in range(n):
+#     for j in range(m):
+#         if mat[i][j] == 0:
+#             row[i] = True
+#             col[j] = True
+
+# print(row)
+# print(col)
+
+# for i in range(n):
+#     for j in range(m):
+#         if row[i] or col[j]:
+#             mat[i][j] = 0
+# print(mat)
+
+
 n = len(mat)
 m = len(mat[0])
 
-row = [False] * n
-col = [False] * m
+col0 = 1
 
 for i in range(n):
     for j in range(m):
         if mat[i][j] == 0:
-            row[i] = True
-            col[j] = True
+            mat[i][0] = 0
 
-print(row)
-print(col)
+            if j == 0:
+                col0 = 0
+            else:
+                mat[0][j] = 0
 
-for i in range(n):
-    for j in range(m):
-        if row[i] or col[j]:
+for i in range(1, n):
+    for j in range(1, m):
+        if mat[i][0] == 0 or mat[0][j] == 0:
             mat[i][j] = 0
+
+if mat[0][0] == 0:
+    for j in range(m):
+        mat[0][j] = 0
+
+if col0 == 0:
+    for i in range(n):
+        mat[i][0] = 0
 print(mat)
