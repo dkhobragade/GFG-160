4eff1c99d2c0ef47221a5c1dc7b1a64d9e2048f8
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-29 20:46:53 +0530

    Refactor QuickSort implementation to improve partitioning logic and enhance clarity

--- Sorting/Quicksort.py
+++ Sorting/Quicksort.py
@@ -35,16 +35,77 @@ high = len(arr) - 1
 # print(arr)
 
 
-def Partition(arr, low, high):
-    i = low - 1
-    pivot = arr[high]
+# def Partition(arr, low, high):
+#     i = low - 1
+#     pivot = arr[high]
 
-    for j in range(low, high):
-        if arr[j] <= pivot:
+#     for j in range(low, high):
+#         if arr[j] <= pivot:
+#             i += 1
+#             arr[i], arr[j] = arr[j], arr[i]
+#     arr[i + 1], arr[high] = arr[high], arr[i + 1]
+#     return i + 1
+
+
+# def QuickSort(arr, low, high):
+#     if low < high:
+#         pivot = Partition(arr, low, high)
+#         QuickSort(arr, low, pivot - 1)
+#         QuickSort(arr, pivot + 1, high)
+
+
+# QuickSort(arr, low, high)
+# print(arr)
+
+
+# low = 0
+# high = len(arr) - 1
+
+
+# def Partition(arr, low, high):
+#     i = low - 1
+#     pivot = arr[high]
+
+#     for j in range(low, high):
+#         if arr[j] < pivot:
+#             i += 1
+#             arr[i], arr[j] = arr[j], arr[i]
+#     arr[i + 1], arr[high] = arr[high], arr[i + 1]
+#     return i + 1
+
+
+# def QuickSort(arr, low, high):
+#     if low < high:
+#         pivot = Partition(arr, low, high)
+#         QuickSort(arr, low, pivot - 1)
+#         QuickSort(arr, pivot + 1, high)
+
+
+# QuickSort(arr, low, high)
+# print(arr)
+
+
+low = 0
+high = len(arr) - 1
+
+
+def Partition(arr, low, high):
+    pivot = arr[low]
+    i = low + 1
+    j = high
+
+    while i <= j:
+        while i <= j and arr[i] <= pivot:
             i += 1
+        while i <= j and arr[j] > pivot:
+            j -= 1
+        if i < j:
             arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+        else:
+            break
+    arr[low], arr[j] = arr[j], arr[low]
+
+    return j
 
 
 def QuickSort(arr, low, high):
@@ -55,4 +116,5 @@ def QuickSort(arr, low, high):
 
 
 QuickSort(arr, low, high)
+
 print(arr)
