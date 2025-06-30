c948cf56fb581338c9274ce6cd9413a886b0be69
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-30 12:44:27 +0530

    Refactor Move_all_negative_numbers_to_beginning implementation by commenting out unused Merge Sort code and enhancing clarity of the negative number sorting logic

--- Miscellaneous/Move_all_negative_numbers_to_beginning.py
+++ Miscellaneous/Move_all_negative_numbers_to_beginning.py
@@ -3,42 +3,59 @@ arr = [-12, 11, -13, -5, 6, -7, 5, -3, -6]
 
 # MERGE SORT
 
+# low = 0
+# high = len(arr) - 1
+
+
+# def Merge(arr, low, mid, high):
+#     left = low
+#     right = mid + 1
+#     temp = []
+
+#     while left <= mid and right <= high:
+#         if arr[left] < arr[right]:
+#             temp.append(arr[left])
+#             left += 1
+#         else:
+#             temp.append(arr[right])
+#             right += 1
+
+#     while left <= mid:
+#         temp.append(arr[left])
+#         left += 1
+
+#     while right <= high:
+#         temp.append(arr[right])
+#         right += 1
+
+#     for i in range(len(temp)):
+#         arr[i + low] = temp[i]
+
+
+# def Divide(arr, low, high):
+#     if low < high:
+#         mid = (low + high) // 2
+#         Divide(arr, low, mid)
+#         Divide(arr, mid + 1, high)
+#         Merge(arr, low, mid, high)
+
+
+# Divide(arr, low, high)
+# print(arr)
+
+
 low = 0
 high = len(arr) - 1
 
 
-def Merge(arr, low, mid, high):
-    left = low
-    right = mid + 1
-    temp = []
+while low < high:
+    while arr[low] < 0:
+        low += 1
+    while arr[high] > 0:
+        high -= 1
 
-    while left <= mid and right <= high:
-        if arr[left] < arr[right]:
-            temp.append(arr[left])
-            left += 1
-        else:
-            temp.append(arr[right])
-            right += 1
-
-    while left <= mid:
-        temp.append(arr[left])
-        left += 1
-
-    while right <= high:
-        temp.append(arr[right])
-        right += 1
-
-    for i in range(len(temp)):
-        arr[i + low] = temp[i]
-
-
-def Divide(arr, low, high):
-    if low < high:
-        mid = (low + high) // 2
-        Divide(arr, low, mid)
-        Divide(arr, mid + 1, high)
-        Merge(arr, low, mid, high)
-
-
-Divide(arr, low, high)
+    if high > low:
+        arr[low], arr[high] = arr[high], arr[low]
+        low += 1
+        high -= 1
 print(arr)
