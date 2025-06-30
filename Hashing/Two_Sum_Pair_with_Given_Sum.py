3cbf920e2e76ee56996bef389445672882064b9f
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-30 10:12:53 +0530

    Refactor Two Sum Pair implementation to remove unused code and enhance clarity by adopting a two-pointer approach

--- Hashing/Two_Sum_Pair_with_Given_Sum.py
+++ Hashing/Two_Sum_Pair_with_Given_Sum.py
@@ -13,28 +13,46 @@ target = 16
 # print(SumPair(arr))
 
 
-def BinarySearch(arr, low, high, req):
-    while low <= high:
-        mid = low + (high - low) // 2
+# def BinarySearch(arr, low, high, req):
+#     while low <= high:
+#         mid = low + (high - low) // 2
 
-        if arr[mid] == req:
-            return True
-        elif arr[mid] < req:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return False
+#         if arr[mid] == req:
+#             return True
+#         elif arr[mid] < req:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+#     return False
 
 
-def SumPair(arr):
+# def SumPair(arr):
+#     arr.sort()
+
+#     for i in range(len(arr)):
+#         req = target - arr[i]
+
+#         if BinarySearch(arr, i + 1, len(arr) - 1, req):
+#             return True
+#     return False
+
+
+# print(SumPair(arr))
+
+
+def TwoPointer(arr):
     arr.sort()
+    left = 0
+    right = len(arr) - 1
 
-    for i in range(len(arr)):
-        req = target - arr[i]
-
-        if BinarySearch(arr, i + 1, len(arr) - 1, req):
+    while left < right:
+        if arr[left] + arr[right] == target:
             return True
+        elif arr[left] + arr[right] > target:
+            right -= 1
+        else:
+            left += 1
     return False
 
 
-print(SumPair(arr))
+print(TwoPointer(arr))
