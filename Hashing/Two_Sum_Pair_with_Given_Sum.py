d8580f120e19cac5bca34e4f3a07367a2d881e9e
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-30 10:08:53 +0530

    Refactor Two Sum Pair implementation to improve efficiency and clarity by using binary search

--- Hashing/Two_Sum_Pair_with_Given_Sum.py
+++ Hashing/Two_Sum_Pair_with_Given_Sum.py
@@ -1,12 +1,39 @@
-arr = [11]
-target = 11
+arr = [1, 4, 45, 6, 10, 8]
+target = 16
+
+
+# def SumPair(arr):
+#     for i in range(1, len(arr)):
+#         for j in range(len(arr) - 1):
+#             if arr[i] + arr[j] == target:
+#                 return True
+#     return False
+
+
+# print(SumPair(arr))
+
+
+def BinarySearch(arr, low, high, req):
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] == req:
+            return True
+        elif arr[mid] < req:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
 
 
 def SumPair(arr):
-    for i in range(1, len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[i] + arr[j] == target:
-                return True
+    arr.sort()
+
+    for i in range(len(arr)):
+        req = target - arr[i]
+
+        if BinarySearch(arr, i + 1, len(arr) - 1, req):
+            return True
     return False
 
 
