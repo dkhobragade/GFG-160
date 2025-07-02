ea582397653043b4b65cf6cd7aa6dd7774cd95fa
dkhobragade <dikshantkhobragade12@gmail.com>
2025-07-02 10:24:18 +0530

    Refactor Find_All_Triplets_with_Zero_Sum implementation by removing commented-out code and enhancing clarity of the triplet finding logic

--- Hashing/Find_All_Triplets_with_Zero_Sum.py
+++ Hashing/Find_All_Triplets_with_Zero_Sum.py
@@ -1,11 +1,35 @@
 arr = [0, -1, 2, -3, 1]
 
-temp = []
-n = len(arr)
+# temp = []
+# n = len(arr)
 
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            if arr[i] + arr[j] + arr[k] == 0:
-                temp.append([i, j, k])
-print(temp)
+# for i in range(n - 2):
+#     for j in range(i + 1, n - 1):
+#         for k in range(j + 1, n):
+#             if arr[i] + arr[j] + arr[k] == 0:
+#                 temp.append([i, j, k])
+# print(temp)
+
+res = set()
+n = len(arr)
+mp = {}
+
+
+for i in range(n):
+    for j in range(i + 1, n):
+        a = arr[i] + arr[j]
+        if a not in mp:
+            mp[a] = []
+        mp[a].append((i, j))
+
+for i in range(n):
+    rem = -arr[i]
+    if rem in mp:
+        for p in mp[rem]:
+            if p[0] != i and p[1] != i:
+                curr = sorted([i, p[0], p[1]])
+                res.add(tuple(curr))
+print(res)
+print([list(triplet) for triplet in res])
+
+print(mp)
