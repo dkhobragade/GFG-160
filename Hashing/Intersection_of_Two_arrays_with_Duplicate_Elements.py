9f6b959f358683575ad20e8a891f9d402e99874c
dkhobragade <dikshantkhobragade12@gmail.com>
2025-07-03 09:34:23 +0530

    Refactor Intersection_of_Two_arrays_with_Duplicate_Elements implementation by removing commented-out code and enhancing clarity of the intersection logic

--- Hashing/Intersection_of_Two_arrays_with_Duplicate_Elements.py
+++ Hashing/Intersection_of_Two_arrays_with_Duplicate_Elements.py
@@ -1,10 +1,21 @@
 a = [1, 2, 1, 3, 1]
 b = [3, 1, 3, 4, 1]
 
-s = []
+# s = []
 
-for i in range(len(a)):
-    if (a[i] in b) and a[i] not in s:
-        s.append(a[i])
+# for i in range(len(a)):
+#     if (a[i] in b) and a[i] not in s:
+#         s.append(a[i])
 
-print(s)
+# print(s)
+
+setA = set(a)
+res = []
+print(setA)
+
+
+for i in b:
+    if i in setA:
+        res.append(i)
+        setA.remove(i)
+print(res)
