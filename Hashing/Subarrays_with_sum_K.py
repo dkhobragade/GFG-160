7080ca22bf8080bfa29a9922dbb9920dfc950aaf
dkhobragade <dikshantkhobragade12@gmail.com>
2025-07-07 20:34:43 +0530

    Refactor subarray sum implementation to use prefix sums for improved efficiency

--- Hashing/Subarrays_with_sum_K.py
+++ Hashing/Subarrays_with_sum_K.py
@@ -2,11 +2,29 @@ arr = [10, 2, -2, -20, 10]
 k = -10
 
 
+# count = 0
+# for i in range(len(arr)):
+#     sum = 0
+#     for j in range(i, len(arr)):
+#         sum += arr[j]
+#         if sum == k:
+#             count += 1
+# print(count)
+
+prefixSum = {}
 count = 0
-for i in range(len(arr)):
-    sum = 0
-    for j in range(i, len(arr)):
-        sum += arr[j]
-        if sum == k:
-            count += 1
+curSum = 0
+
+
+for i in arr:
+    curSum += i
+
+    if curSum == k:
+        count += 1
+
+    if curSum - k in prefixSum:
+        count += prefixSum[curSum - k]
+
+    prefixSum[curSum] = prefixSum.get(curSum, 0) + 1
+
 print(count)
