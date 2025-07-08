1c423d5ee11b98fbcbb6ead18ab250b4b1b3ed87
dkhobragade <dikshantkhobragade12@gmail.com>
2025-07-08 11:52:43 +0530

    Refactor Count_Subarrays_with_given_XOR implementation to remove commented-out code and improve clarity

--- Hashing/Count_Subarrays_with_given_XOR.py
+++ Hashing/Count_Subarrays_with_given_XOR.py
@@ -2,13 +2,30 @@ arr = [4, 2, 2, 6, 4]
 k = 6
 
 
+# count = 0
+
+# for i in range(len(arr)):
+#     ans = 0
+#     for j in range(i, len(arr)):
+#         ans ^= arr[j]
+
+#         if ans == k:
+#             count += 1
+# print(count)
+
 count = 0
+mp = {}
+pefixXOR = 0
 
-for i in range(len(arr)):
-    ans = 0
-    for j in range(i, len(arr)):
-        ans ^= arr[j]
 
-        if ans == k:
-            count += 1
+for val in arr:
+    pefixXOR ^= val
+
+    count += mp.get(pefixXOR ^ k, 0)
+
+    if pefixXOR == k:
+        count += 1
+
+    mp[pefixXOR] = mp.get(pefixXOR, 0) + 1
+
 print(count)
