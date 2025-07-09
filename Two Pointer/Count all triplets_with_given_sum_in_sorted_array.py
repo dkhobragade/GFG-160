bab571d985f9253797cd98370e46a0127653bf82
dkhobragade <dikshantkhobragade12@gmail.com>
2025-07-09 10:33:28 +0530

    Refactor triplet counting logic to improve clarity and efficiency by removing unused code and simplifying conditions

--- Two Pointer/Count all triplets_with_given_sum_in_sorted_array.py
+++ Two Pointer/Count all triplets_with_given_sum_in_sorted_array.py
@@ -13,20 +13,56 @@ count = 0
 # print(count)
 
 
-for i in range(n):
-    newTarget = arr[i] - target
+# for i in range(n):
+#     newTarget = arr[i] - target
+
+#     left = i + 1
+#     right = n - 1
+
+#     while left < right:
+#         if arr[left] + arr[right] == target:
+#             count += 1
+#             left += 1
+#             right -= 1
+#         elif arr[left] + arr[right] < target:
+#             left += 1
+#         else:
+#             right -= 1
+
+# print(count)
+
+
+for i in range(n - 2):
 
     left = i + 1
     right = n - 1
 
     while left < right:
-        if arr[left] + arr[right] == target:
-            count += 1
+
+        sum = arr[left] + arr[right] + arr[i]
+
+        if sum < target:
             left += 1
+        elif sum > target:
             right -= 1
-        elif arr[left] + arr[right] < target:
-            left += 1
         else:
-            right -= 1
+            ele1 = arr[left]
+            ele2 = arr[right]
+
+            cnt1 = 0
+            cnt2 = 0
+
+            while left <= right and arr[left] == ele1:
+                cnt1 += 1
+                left += 1
+
+            while left <= right and arr[right] == ele2:
+                cnt2 += 1
+                right -= 1
+
+            if ele1 == ele2:
+                count += (cnt1 * (cnt1 - 1)) // 2
+            else:
+                count += cnt1 * cnt2
 
 print(count)
