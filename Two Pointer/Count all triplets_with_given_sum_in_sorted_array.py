adb6563b16dabca4a2e9ec67d9ee4b0c3839c5df
dkhobragade <dikshantkhobragade12@gmail.com>
2025-07-09 10:03:25 +0530

    Refactor triplet counting implementation to use two-pointer technique for improved efficiency

--- Two Pointer/Count all triplets_with_given_sum_in_sorted_array.py
+++ Two Pointer/Count all triplets_with_given_sum_in_sorted_array.py
@@ -5,9 +5,28 @@ target = -2
 n = len(arr)
 count = 0
 
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         for k in range(j + 1, n):
+#             if arr[i] + arr[j] + arr[k] == target:
+#                 count += 1
+# print(count)
+
+
 for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            if arr[i] + arr[j] + arr[k] == target:
-                count += 1
+    newTarget = arr[i] - target
+
+    left = i + 1
+    right = n - 1
+
+    while left < right:
+        if arr[left] + arr[right] == target:
+            count += 1
+            left += 1
+            right -= 1
+        elif arr[left] + arr[right] < target:
+            left += 1
+        else:
+            right -= 1
+
 print(count)
