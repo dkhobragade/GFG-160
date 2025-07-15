8ee4c675855b3043f18dca7593fb7e8a014fae82
Dikshant <dikshantkhobragade12@gmail.com>
2025-07-15 18:49:50 +0530

    count distnct element in the every window with the dict

--- Two Pointer/Count_distinct_elements_in_every_window.py
+++ Two Pointer/Count_distinct_elements_in_every_window.py
@@ -3,22 +3,51 @@ from collections import defaultdict
 arr = [1, 2, 1, 3, 4, 2, 3]
 k = 4
 
+# res = []
+# freq = defaultdict(int)
+
+# for i in range(k):
+#     freq[arr[i]] += 1
+
+# res.append(len(freq))
+
+# for i in range(k, len(arr)):
+#     freq[arr[i]] += 1
+#     freq[arr[i - k]] -= 1
+
+#     if freq[arr[i - k]] == 0:
+#         del freq[arr[i - k]]
+
+#     res.append(len(freq))
+# print(res)
+
+n = len(arr)
 res = []
-freq = defaultdict(int)
+
+freq = {}
 
 for i in range(k):
-    freq[arr[i]] += 1
+    if arr[i] in freq:
+        freq[arr[i]] += 1
+    else:
+        freq[arr[i]] = 1
 
-print(freq)
 res.append(len(freq))
 
-for i in range(k, len(arr)):
-    freq[arr[i]] += 1
-    freq[arr[i - k]] -= 1
 
-    if freq[arr[i - k]] == 0:
-        del freq[arr[i - k]]
+for i in range(k, n):
+    outgoing = arr[i - k]
+    freq[outgoing] -= 1
+
+    if freq[outgoing] == 0:
+        del freq[outgoing]
+
+    incoming = arr[i]
+    if incoming in freq:
+        freq[incoming] += 1
+    else:
+        freq[incoming] = 1
 
     res.append(len(freq))
-print(freq)
+
 print(res)
