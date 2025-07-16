6527bf6892e07d6557495f93292cd7e9c26314a6
Dikshant <dikshantkhobragade12@gmail.com>
2025-07-16 13:00:36 +0530

    Longest substring with distinct characters

---
+++ Two Pointer/Longest_substring_with_distinct_characters.py
@@ -0,0 +1,16 @@
+s = "geeksforgeeks"
+
+
+freq = {}
+left = 0
+max_len = 0
+
+
+for right in range(len(s)):
+    if s[right] in freq and freq[s[right]] >= left:
+        left = freq[s[right]] + 1
+    freq[s[right]] = right
+    max_len = max(max_len, right - left + 1)
+
+print(freq)
+print(max_len)
